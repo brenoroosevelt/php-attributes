54f2d77f78b50fe52f284f7e40a34f04d13d96e0
Breno Roosevelt <breno.jesus@ufms.br>
2022-01-27 09:59:08 -0400

    cleanup

--- src/ParsedAttribute.php
+++ src/ParsedAttribute.php
@@ -3,9 +3,11 @@ declare(strict_types=1);
 
 namespace BrenoRoosevelt\PhpAttributes;
 
+use Attribute;
 use ReflectionAttribute;
 use ReflectionClass;
 use ReflectionClassConstant;
+use ReflectionFunction;
 use ReflectionMethod;
 use ReflectionParameter;
 use ReflectionProperty;
@@ -15,10 +17,11 @@ final class ParsedAttribute
     public function __construct(
         public readonly ReflectionAttribute $attribute,
         public readonly
-        ReflectionClass|
-        ReflectionClassConstant|
-        ReflectionProperty|
-        ReflectionMethod|
+        ReflectionClass |
+        ReflectionClassConstant |
+        ReflectionProperty |
+        ReflectionMethod |
+        ReflectionFunction |
         ReflectionParameter $target
     ) {
     }
